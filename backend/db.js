40ed5ad4f6f00169bbe5529e11ac9da432f28f64
agarwalayush9 <agarwalayush991@gmail.com>
2023-07-20 00:22:12 +0530

    .env changes

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURI="mongodb://localhost:27017";
-
+const mongoURI="mongodb+srv://ayush_cse:$Ayush6677@ayush-cse.uokcc3u.mongodb.net/?retryWrites=true&w=majority";
+require('dotenv').config();
 const connectToMongo =async ()=>{
     try{
         mongoose.connect(mongoURI)
+        console.log(process.env.REACT_APP_DB)
         console.log("Connected To Mongo - Success...");
     }
     catch(e){
