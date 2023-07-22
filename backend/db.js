1f996f15ef89cf6f5d9fcb05f2a31f3ce0f406f0
agarwalayush9 <agarwalayush991@gmail.com>
2023-07-22 13:01:57 +0530

    vercel

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
-const mongoURI="mongodb+srv://ayush_cse:$Ayush6677@ayush-cse.uokcc3u.mongodb.net/?retryWrites=true&w=majority";
 require('dotenv').config();
+const mongoURI=process.env.REACT_APP_INVOICE_DATA;
 const connectToMongo =async ()=>{
     try{
-        mongoose.connect(mongoURI)
-        console.log(process.env.REACT_APP_DB)
+        mongoose.connect(mongoURI);
         console.log("Connected To Mongo - Success...");
     }
     catch(e){
