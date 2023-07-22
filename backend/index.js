eee67f28c716db544fb0009ad21855df4274b0eb
agarwalayush9 <agarwalayush991@gmail.com>
2023-07-22 13:33:32 +0530

    jhk

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ var cors = require('cors')
 
 connectToMongo();
 const app = express();
-const port = 5000;
+const port = 'https://ibox-api.vercel.app/';
 
 // app.get('/',(req,res)=>{
   //   res.send('Hi founder and Ceo')
