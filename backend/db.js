bedac9c5800cf45cc4b7f62f818de17318c344b2
agarwalayush9 <agarwalayush991@gmail.com>
2023-07-23 23:24:34 +0530

    uri

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const mongoURI=process.env.MONGODB_URI;
+// const mongoURI=process.env.MONGODB_URI;
 const connectToMongo =async ()=>{
     try{
-        mongoose.connect(mongoURI);
+        mongoose.connect('mongodb+srv://ayush_cse:$Ayush6677@ayush-cse.uokcc3u.mongodb.net/?retryWrites=true&w=majority');
         console.log("Connected To Mongo - Success...");
     }
     catch(e){
