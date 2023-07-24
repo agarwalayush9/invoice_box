89e5b4592753701a5b79970606df7e9cb07f718e
agarwalayush9 <agarwalayush991@gmail.com>
2023-07-24 19:37:48 +0530

    url

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // const mongoURI=process.env.REACT_APP_INVOICE_DATA;
 const connectToMongo =async ()=>{
     try{
-        mongoose.connect('mongodb+srv://ayush_cse:$Ayush6677@ayush-cse.uokcc3u.mongodb.net/?retryWrites=true&w=majority');
+        mongoose.connect(process.env.MONGODB_URL);
         console.log("Connected To Mongo - Success...");
     }
     catch(e){
